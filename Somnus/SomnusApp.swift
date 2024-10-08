fbada7207fd024aa05b2fe2d8998c116e54e9ac2
Vnt0n <antoine.chosson@gmail.com>
2024-10-08 08:39:01 +0200

    git init

---
+++ Somnus/SomnusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomnusApp.swift
+//  Somnus
+//
+//  Created by Antoine on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SomnusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
