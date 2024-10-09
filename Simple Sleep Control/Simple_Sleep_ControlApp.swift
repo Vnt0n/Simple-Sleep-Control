46d396b860f277934f31e978127f2404e5df2e31
Vnt0n <antoine.chosson@gmail.com>
2024-10-09 16:35:07 +0200

    OpeningView

--- Simple Sleep Control/Simple_Sleep_ControlApp.swift
+++ Simple Sleep Control/Simple_Sleep_ControlApp.swift
@@ -15,7 +15,7 @@ struct SimpleSleepControlApp: App {
 
     var body: some Scene {
         WindowGroup {
-            AboutMeView()
+            OpeningView()
         }
         MenuBarExtra {
             VStack {
@@ -81,6 +81,41 @@ struct SimpleSleepControlApp: App {
     }
 }
 
+// Vue d'ouverture
+struct OpeningView: View {
+    @Environment(\.dismiss) var dismiss  // Ajoute l'action dismiss
+
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            Text("Simple Sleep Control")
+                .font(.title)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                dismiss()
+            }) {
+                Text("OK")
+                    .font(.title3)
+                    .bold()
+                    .padding()
+                    .frame(width: 70)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            Spacer()
+        }
+        .frame(minWidth: 400, minHeight: 200)
+    }
+}
+
 // Vue pour l'onglet "About me"
 struct AboutMeView: View {
     @Environment(\.dismiss) var dismiss  // Ajoute l'action dismiss
@@ -90,7 +125,7 @@ struct AboutMeView: View {
             
             Spacer()
             
-            Text("Simple Sleep Control")
+            Text("About me")
                 .font(.title)
                 .padding()
             
